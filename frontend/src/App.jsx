cdfbe39b42d0b50b197cfcb8022f180d8333973c
ankitmindfiretraining-99 <ankitmindfiretraining@gmail.com>
2025-08-08 11:06:52 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { FileUploader } from './components/FileUploader'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <FileUploader/>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
